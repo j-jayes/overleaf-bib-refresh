a9180f94dc5e6508d02e70934937fa79be19253f
Jonathan Jayes <j0nathanjayes@gmail.com>
2025-07-03 13:08:29 +0200

    change trigger

--- src/refresh.py
+++ src/refresh.py
@@ -21,7 +21,7 @@ def setup_driver():
     Setup the Selenium WebDriver.
     """
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Uncomment this to run in headless mode for CRON jobs
+    # chrome_options.add_argument("--headless")  # Uncomment this to run in headless mode for CRON jobs
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-notifications")
